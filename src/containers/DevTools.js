647d056ccfb821e2edb1c8f08d3ad99854bf9ee7
LBenzahia <lakhdar.benzahia@gmail.com>
2017-06-26 10:46:33 +0100

    Setup Redux

---
+++ src/containers/DevTools.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createDevTools } from 'redux-devtools'; // eslint-disable-line import/no-extraneous-dependencies
+import LogMonitor from 'redux-devtools-log-monitor'; // eslint-disable-line import/no-extraneous-dependencies
+import DockMonitor from 'redux-devtools-log-monitor' // eslint-disable-line import/no-extraneous-dependencies
+
+export default createDevTools(
+  <DockMonitor
+    toggleVisibilityKey="ctrl-h"
+    changePositionKey="ctrl-w"
+  >
+    <LogMonitor />
+  <DockMonitor />
+);
