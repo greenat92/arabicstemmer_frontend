e4b09ce10111e5306a1767ea579bf367da05e483
LBenzahia <lakhdar.benzahia@gmail.com>
2017-06-27 09:29:03 +0100

    Reorgonize/Setup Redux

---
+++ src/containers/Root.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+
+import App from './App/App';
+import DevTools from './DevTools';
+
+const Root = ({}) => (
+  <MuiThemeProvider>
+     <Provider store={store}>
+       <div>
+         <App />
+         <DevTools />
+       </div>
+     </Provider>
+  </MuiThemeProvider>
+);
+
+Root.propTypes = {
+  store: PropTypes.shape({ /* TODO: update */ }).isRequired,
+  history: PropTypes.shape({ /* TODO: update */ }).isRequired,
+};
+
+export default Root;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import './style/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
