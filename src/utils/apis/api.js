6cafb9fa3b65dc915de4d937076fd4cbc56cc847
LBenzahia <lakhdar.benzahia@gmail.com>
2017-06-29 12:54:47 +0100

    added utils

---
+++ src/utils/apis/api.js
@@ -0,0 +1,6 @@
+// @flow
+
+//https://github.com/LBenzahia/arabicstemmer_webapp/blob/master/assets/js/actions/index.js
+// TODO: Consider using stemming api's (as in the example above)
+
+const BASE_URL = 'http://localhost:3001';
