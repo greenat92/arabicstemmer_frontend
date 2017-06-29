85e7a0ca03de4a615064b7b83439fa04ac2ceab4
LBenzahia <lakhdar.benzahia@gmail.com>
2017-06-29 23:12:23 +0100

    prepar title and logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -10,8 +10,18 @@ export default class Header extends React.Component {
   render() {
     return (
       <div className="App-header">
-            <ul className="App-menu">
-            </ul>
+              <div className="App-wraper">
+                    <div className="App-logo-space">
+                        <a>
+                          <img src="" className="App-logo" />
+                          <div className="App-title"> </div>
+                        </a>
+                    </div>
+                    <ul className="App-menu">
+                    <li className="App-item"><a>Code</a></li>
+                    <li className="App-item"><a>Corpura</a></li>
+                    </ul>
+          </div>
       </div>
 
     );
