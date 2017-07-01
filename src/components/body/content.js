b8a00a5c39852f75a5e5057bd17df3942655057f
LBenzahia <lakhdar.benzahia@gmail.com>
2017-07-01 21:09:55 +0100

    Build content

--- src/components/body/content.js
+++ src/components/body/content.js
@@ -13,12 +13,25 @@ export default class ContentPage extends React.Component {
     return (
       <div className="App-content">
           <div className="App-description">
+          <h2>Description</h2>
+           <p>
+               Welcome to the Arabic Light Stemming Algorithm made for Snowball,
+               it s fast and can be generated in many programming languages
+               (through Snowball).
+           </p>
           </div>
           <div className="App-demo">
+            <h2>Demo</h2>
+              <p>
+                  Type some Arabic text and press "Stem!" button or "File" to read from a local ".txt" file
+              </p>
           </div>
           <div className="App-code">
+              <h2>Code</h2>
+
           </div>
           <div className="App-contribute">
+             <h2>Contributors</h2>
           </div>
       </div>
 
